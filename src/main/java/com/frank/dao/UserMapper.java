af656149f7f1470f4421247a4cd1b08f72cd4222
frank <530438645@qq.com>
2016-10-02 23:19:45 +0800

    补上dao文件

---
+++ src/main/java/com/frank/dao/UserMapper.java
@@ -0,0 +1,14 @@
+package com.frank.dao;
+
+import com.frank.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+}
